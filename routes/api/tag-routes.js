51e585e29836941221096fa1543070ba0323eae7
Fifinella <h.hoesley@gmail.com>
2023-04-19 16:36:18 -0700

    added missing put route in tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,6 +46,24 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  // update a tag by its `id` value
+  try {
+    const updatedTag = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+
+    if (!updatedTag[0]) {
+      res.status(404).json({ message: "No tag with that id." });
+      return;
+    }
+
+    res.status(200).json(updatedTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
